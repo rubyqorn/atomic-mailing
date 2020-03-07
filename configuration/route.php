eea27c7dafb6be3a14151c427fa6b04b6577bc63
Anton Hideger <antonhideger1337@gmail.com>
2020-03-07 10:01:40 +0300

    created configuration, htaccess and routes files

---
+++ configuration/route.php
@@ -0,0 +1,11 @@
+<?php 
+
+# Here we can configure our routes 
+
+# By default you have a php extension, but if you will 
+# want to display your routes in routes.yml or routes.js
+# file you can set a "js" or yaml extension
+
+$extension = [
+    "extension_name" => "php"
+];
