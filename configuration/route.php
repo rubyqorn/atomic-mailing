e52bb062c9d3006e770737a362efb64dd0c77314
Anton Hideger <antonhideger1337@gmail.com>
2020-03-10 03:23:56 +0300

    realized route transformator

--- configuration/route.php
+++ configuration/route.php
@@ -7,5 +7,13 @@
 # file you can set a "json" or "yaml" extension
 
 $extension = [
-    "extension_name" => "yml"
+    "extension_name" => "json"
+];
+
+# Route parameters which will be use for flexible route
+# parameters manipulations. If you will to set own pattern,
+# you can make it like this "{key}" => "regular expression pattern"
+
+$route_parameters = [
+    "{id}" => "([0-9+])"
 ];
--- core/Routing/RouteTransformator.php
+++ core/Routing/RouteTransformator.php
@@ -2,9 +2,98 @@
 
 namespace Atomic\Core\Routing;
 
-use Atomic\Core\Exceptions\InvalidRouteName;
-
 class RouteTransformator extends RouteConfigurator
 {
-   //
+    /**
+     * Routes from file
+     * 
+     * @var array
+     */ 
+    protected $routes;
+
+    /**
+     * Route parameters
+     * 
+     * @var array 
+     */  
+    protected $configuredParameters;
+
+    /**
+     * Routes with replaced 
+     * patterns
+     * 
+     * @var array
+     */ 
+    protected $parsedRoutes;
+
+    /**
+     * Matched route with URI
+     * 
+     * @var string
+     */ 
+    protected $mathchedRoute;
+
+    /**
+     * Initialize routes
+     * 
+     * @return array
+     */ 
+    private function buildConfiguration()
+    {
+        return $this->configurate();
+    }
+
+    /**
+     * Get route configuration file where
+     * contains route parameters settings
+     * 
+     * @return array
+     */ 
+    private function getRouteConfiguredParameters()
+    {
+        require '../configuration/route.php';
+        return $route_parameters;
+    }
+
+    /**
+     * Search route parameter and replace it
+     * on pattern. 
+     * Example: delete {id} and set ([0-9+])
+     * 
+     * @return array
+     */ 
+    protected function replaceRouteParameters()
+    {
+        $this->routes = $this->buildConfiguration();
+
+        $this->configuredParameters = $this->getRouteConfiguredParameters();
+
+        $routes = [];
+
+        foreach ($this->configuredParameters as $param => $value) {
+            foreach($this->routes as $routeName => $controller) {
+                $routes[$routeName] = str_replace($param, $value, $routeName);
+            }
+        }
+
+        return $routes;
+    }
+
+    /**
+     * Match request URI with pattern from array
+     * 
+     * @return string
+     */ 
+    protected function matchWithRequestUri()
+    {
+        $this->parsedRoutes = $this->replaceRouteParameters();
+
+        foreach($this->parsedRoutes as $route => $pattern) {
+            if (preg_match("#{$pattern}#", $_SERVER['REQUEST_URI'])) {
+                return $route;
+            }
+
+        }        
+    }
+
 }
