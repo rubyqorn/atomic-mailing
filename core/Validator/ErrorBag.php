7008b0bcff387b841a61118363e499324c072b88
Anton Hideger <antonhideger1337@gmail.com>
2020-04-04 13:57:42 +0300

    realized error bag class

--- core/Validator/ErrorBag.php
+++ core/Validator/ErrorBag.php
@@ -4,5 +4,48 @@ namespace Atomic\Core\Validator;
 
 class ErrorBag
 {
-    //
+    /**
+     * Error messages
+     * 
+     * @var array
+     */ 
+    protected array $messages = [];
+
+    /**
+     * Record message into messages array
+     * 
+     * @param string $message 
+     * 
+     * @return array
+     */ 
+    public function recording(string $message)
+    {
+        return $this->messages[] = $message;
+    }
+
+    /**
+     * Return list of exists messages
+     * 
+     * @return array
+     */ 
+    public function getAll()
+    {
+        return $this->messages;
+    }
+
+    /**
+     * Get last error from array
+     * 
+     * @return string
+     */ 
+    protected function lastError()
+    {
+        $lastError = array_pop($this->messages);
+        return $lastError;
+    }
+
+    public function __toString()
+    {
+        return $this->lastError();
+    }
 }
